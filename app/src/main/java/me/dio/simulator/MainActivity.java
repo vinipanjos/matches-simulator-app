8325373800fe333c982d1ebcc7e52978d93731f2
Vinicius <vinipanjos@hotmail.com>
2022-03-17 16:03:24 -0300

    App de Simulação finalizado

--- app/src/main/java/me/dio/simulator/MainActivity.java
+++ app/src/main/java/me/dio/simulator/MainActivity.java
@@ -1,20 +1,115 @@
 package me.dio.simulator;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
+import java.util.Random;
+
+import data.MatchesApi;
+import me.dio.simulator.adapter.MatchesAdapter;
 import me.dio.simulator.databinding.ActivityMainBinding;
+import me.dio.simulator.domain.Match;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private MatchesApi matchesApi;
+    private MatchesAdapter matchesAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        SetupHttpClient();
+        SetupMatchesList();
+        SetupMatchesRefresh();
+        SetupFloatingActionButton();
+
+    }
+
+    private void SetupHttpClient() {
+        Retrofit retroFit = new Retrofit.Builder()
+                .baseUrl("https://vinipanjos.github.io/matches-simulator-api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        matchesApi = retroFit.create(MatchesApi.class);
+    }
+
+    private void SetupMatchesList() {
+        //TODO Listar as partidas consumindo a API que criei no github
+        binding.rvMatches.setHasFixedSize(true);
+        binding.rvMatches.setLayoutManager(new LinearLayoutManager(this));
+        findMatchesFromApi();
+    }
+
+    private void SetupMatchesRefresh() {
+        //TODO Fazer a atualização na ação de swipe
+        binding.srlMatches.setOnRefreshListener(this::findMatchesFromApi);
+    }
+
+    private void SetupFloatingActionButton() {
+        //TODO Criar evento de clique para simular a partida
+        binding.fabSimulate.setOnClickListener(view -> {
+            view.animate().rotation(360).setDuration(1000).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    for (int i = 0; i < matchesAdapter.getItemCount(); i++) {
+                        Random random = new Random();
+                        Match match = matchesAdapter.getMatches().get(i);
+                        match.getHomeTeam().setScore(random.nextInt(match.getHomeTeam().getStars() + 1));
+                        match.getAwayTeam().setScore(random.nextInt(match.getAwayTeam().getStars() + 1));
+                        matchesAdapter.notifyItemChanged(i);
+                    }
+                }
+            });
+        });
+    }
+
+    private void findMatchesFromApi() {
+        binding.srlMatches.setRefreshing(true);
+
+        matchesApi.getMatches().enqueue(new Callback<List<Match>>() {
+            @Override
+            public void onResponse(Call<List<Match>> call, Response<List<Match>> response) {
+                if (response.isSuccessful()) {
+                    List<Match> matches = response.body();
+                    matchesAdapter = new MatchesAdapter(matches);
+                    binding.rvMatches.setAdapter(matchesAdapter);
+                } else {
+                    showErrorMessage();
+                }
+                binding.srlMatches.setRefreshing(false);
+            }
+
+            @Override
+            public void onFailure(Call<List<Match>> call, Throwable t) {
+                showErrorMessage();
+                binding.srlMatches.setRefreshing(false);
+            }
+        });
+    }
+
+
+    private void showErrorMessage() {
+        //TODO mostrar uma msgm de erro
+        Snackbar.make(binding.fabSimulate, R.string.error_api, Snackbar.LENGTH_LONG).show();
     }
 }
---
+++ app/src/main/java/me/dio/simulator/adapter/MatchesAdapter.java
@@ -0,0 +1,77 @@
+package me.dio.simulator.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import me.dio.simulator.DetailActivity;
+import me.dio.simulator.databinding.MatchItemBinding;
+import me.dio.simulator.domain.Match;
+
+public class MatchesAdapter extends RecyclerView.Adapter<MatchesAdapter.ViewHolder> {
+
+    private List<Match> matches;
+
+    public MatchesAdapter(List<Match> matches) {
+        this.matches = matches;
+    }
+
+    public List<Match> getMatches() {
+        return matches;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        MatchItemBinding binding = MatchItemBinding.inflate(layoutInflater, parent, false);
+        return new ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Match match = matches.get(position);
+        Context context = holder.itemView.getContext();
+
+
+        //Adapta os dados da nossa partida (recuperadas da api) no nosso layout
+        Glide.with(context).load(match.getHomeTeam().getImage()).circleCrop().into(holder.binding.ivHomeTeam);
+        holder.binding.tvHomeTeamName.setText(match.getHomeTeam().getName());
+        if (match.getHomeTeam().getScore() != null)
+            holder.binding.tvHomeTeamScore.setText(String.valueOf(match.getHomeTeam().getScore()));
+        Glide.with(context).load(match.getAwayTeam().getImage()).circleCrop().into(holder.binding.ivAwayTeam);
+        holder.binding.tvAwayTeamName.setText(match.getAwayTeam().getName());
+        if (match.getAwayTeam().getScore() != null)
+            holder.binding.tvAwayTeamScore.setText(String.valueOf(match.getAwayTeam().getScore()));
+
+        //configurando o clique para a nova tela
+        holder.itemView.setOnClickListener(view -> {
+            Intent intent = new Intent(context, DetailActivity.class);
+            intent.putExtra(DetailActivity.Extras.MATCH, match);
+            context.startActivity(intent);
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return matches.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final MatchItemBinding binding;
+
+        public ViewHolder(MatchItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+}
